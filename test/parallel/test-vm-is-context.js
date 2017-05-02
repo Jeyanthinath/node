d4c1d3766c3da022d6f73160b4b86236052d22a8
jeyanthinath <jeyanthinath10@gmail.com>
2017-05-02 14:55:10 +0530

    test-vm-is-context.js : changed the assert.thows object to regex and added score for sanbox object

--- test/parallel/test-vm-is-context.js
+++ test/parallel/test-vm-is-context.js
@@ -26,7 +26,11 @@ const vm = require('vm');
 
 assert.throws(function() {
   vm.isContext('string is not supported');
+<<<<<<< 8db39971b768118ce2c0c34d88788daea14f52c9
 }, /^TypeError: sandbox must be an object$/);
+=======
+}, /TypeError/);
+>>>>>>> test-vm-is-context.js : changed the assert.thows object to regex and added score for sanbox object
 
 assert.strictEqual(vm.isContext({}), false);
 assert.strictEqual(vm.isContext([]), false);
@@ -34,6 +38,8 @@ assert.strictEqual(vm.isContext([]), false);
 assert.strictEqual(vm.isContext(vm.createContext()), true);
 assert.strictEqual(vm.isContext(vm.createContext([])), true);
 
-const sandbox = { foo: 'bar' };
-vm.createContext(sandbox);
-assert.strictEqual(vm.isContext(sandbox), true);
+{
+  const sandbox = { foo: 'bar' };
+  vm.createContext(sandbox);
+  assert.strictEqual(vm.isContext(sandbox), true);
+}
